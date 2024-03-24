8e23d899caa28e9786180658fe8362e1b95a350d
Fologan <164580328+Fologan@users.noreply.github.com>
2024-03-23 19:06:30 -0600

    Update feed-demo.js
    Los cambios principales son:
    
    La función loadScript ahora recibe un segundo parámetro callback que se ejecuta cuando el script se carga correctamente.
    La función de devolución de llamada para procesar los datos del feed ahora se define dentro de la función callback de loadScript.
    Se ha eliminado la asignación de la función de devolución de llamada a window[ifeeds_${feedLabel}] ya que era redundante.
    Con estos cambios, cada instancia de Feeds.initPlugin debería cargar y procesar correctamente el feed correspondiente a la etiqueta especificada en el parámetro label.

--- feed-demo.js
+++ feed-demo.js
@@ -74,138 +74,139 @@ var Feeds = function() {
     const feedsContainer = document.querySelector(settings.container);
     const feedLabel = settings.label;
 
-    function loadScript(src) {
+    function loadScript(src, callback) {
       const script = document.createElement("script");
       script.src = src;
-      document.body.appendChild(script).parentNode.removeChild(script);
+      script.onload = callback;
+      document.body.appendChild(script);
     }
 
-    loadScript(`${settings.url}feeds/posts/default${feedLabel ? `/-/${feedLabel}` : ""}?alt=json-in-script&callback=window.Feeds.callbacks['${feedLabel}']&max-results=${settings.max}`);
+    loadScript(`${settings.url}feeds/posts/default${feedLabel ? `/-/${feedLabel}` : ""}?alt=json-in-script&max-results=${settings.max}`, function() {
+      window.Feeds.callbacks = window.Feeds.callbacks || {};
 
-    window.Feeds.callbacks = window.Feeds.callbacks || {};
+      window.Feeds.callbacks[feedLabel] = function(data) {
+        if (data.feed.entry && feedsContainer) {
+          if (settings.styles) {
+            injectStyles(`
+              .widget-feeds {
+                --feeds-margin: 1rem;
+                --feeds-gap: 1.25rem;
+                --feeds-border: ${settings.border};
+                padding: var(--feeds-gap);
+                display: flex;
+                max-width: 100%;
+                border: 1px solid var(--feeds-border);
+                border-radius: .5rem;
+                background: var(--feeds-background, ${settings.background});
+              }
 
-    window.Feeds.callbacks[feedLabel] = function(data) {
-      if (data.feed.entry && feedsContainer) {
-        if (settings.styles) {
-          injectStyles(`
-            .widget-feeds {
-              --feeds-margin: 1rem;
-              --feeds-gap: 1.25rem;
-              --feeds-border: ${settings.border};
-              padding: var(--feeds-gap);
-              display: flex;
-              max-width: 100%;
-              border: 1px solid var(--feeds-border);
-              border-radius: .5rem;
-              background: var(--feeds-background, ${settings.background});
-            }
+              .widget-feeds,
+              .widget-feeds * {
+                box-sizing: border-box;
+              }
 
-            .widget-feeds,
-            .widget-feeds * {
-              box-sizing: border-box;
-            }
+              .feeds-item {
+                display: flex;
+                align-items: flex-start;
+              }
 
-            .feeds-item {
-              display: flex;
-              align-items: flex-start;
-            }
+              .feeds-column,
+              .feeds-row .feeds-item {
+                flex-direction: column;
+              }
 
-            .feeds-column,
-            .feeds-row .feeds-item {
-              flex-direction: column;
-            }
+              .feeds-column .feeds-header {
+                margin-right: var(--feeds-margin);
+                width: 80px;
+              }
 
-            .feeds-column .feeds-header {
-              margin-right: var(--feeds-margin);
-              width: 80px;
-            }
+              .feeds-row .feeds-header {
+                margin-bottom: var(--feeds-margin);
+                width: 100%;
+                --feeds-ratio-y: 9;
+                --feeds-ratio-x: 16;
+              }
 
-            .feeds-row .feeds-header {
-              margin-bottom: var(--feeds-margin);
-              width: 100%;
-              --feeds-ratio-y: 9;
-              --feeds-ratio-x: 16;
-            }
+              .feeds-header {
+                flex: none;
+                position: relative;
+                overflow: hidden;
+                border-radius: 0.5rem;
+              }
+              
+              .feeds-header::before {
+                content: "";
+                display: block;
+                padding-top: calc(var(--feeds-ratio-y, 1)/ var(--feeds-ratio-x, 1) * 100%);
+              }
+              
+              .feeds-link {
+                text-decoration: none;
+                color: var(--feeds-title-color, ${settings.title});
+                font-weight: 500;
+                font-size: 1rem;
+                margin-top: .5rem;
+                display: block;
+              }
 
-            .feeds-header {
-              flex: none;
-              position: relative;
-              overflow: hidden;
-              border-radius: 0.5rem;
-            }
-            
-            .feeds-header::before {
-              content: "";
-              display: block;
-              padding-top: calc(var(--feeds-ratio-y, 1)/ var(--feeds-ratio-x, 1) * 100%);
-            }
-            
-            .feeds-link {
-              text-decoration: none;
-              color: var(--feeds-title-color, ${settings.title});
-              font-weight: 500;
-              font-size: 1rem;
-              margin-top: .5rem;
-              display: block;
-            }
+              .feeds-category {
+                text-transform: capitalize;
+                text-decoration: none;
+                display: inline-block;
+                border-radius: 5rem;
+                padding: .25rem .875rem;
+                background-color: var(--feeds-category-bg, ${settings.categorybg});
+                color: var(--feeds-category-color, ${settings.category});
+                font-size: .875rem;
+                font-weight: 400;
+              }
 
-            .feeds-category {
-              text-transform: capitalize;
-              text-decoration: none;
-              display: inline-block;
-              border-radius: 5rem;
-              padding: .25rem .875rem;
-              background-color: var(--feeds-category-bg, ${settings.categorybg});
-              color: var(--feeds-category-color, ${settings.category});
-              font-size: .875rem;
-              font-weight: 400;
-            }
+              .feeds-image {
+                object-fit: cover;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                width: 100%;
+                min-height: 100%;
+                transform: translate(-50%, -50%);
+              }
 
-            .feeds-image {
-              object-fit: cover;
-              position: absolute;
-              top: 50%;
-              left: 50%;
-              width: 100%;
-              min-height: 100%;
-              transform: translate(-50%, -50%);
-            }
+              .feeds-column .feeds-item + * {
+                margin-top: var(--feeds-gap);
+                padding-top: var(--feeds-gap);
+                border-top: 1px solid var(--feeds-border);
+              }
 
-            .feeds-column .feeds-item + * {
-              margin-top: var(--feeds-gap);
-              padding-top: var(--feeds-gap);
-              border-top: 1px solid var(--feeds-border);
-            }
+              .feeds-row .feeds-item + * {
+                margin-left: var(--feeds-gap);
+              }
 
-            .feeds-row .feeds-item + * {
-              margin-left: var(--feeds-gap);
-            }
+              .feeds-row {
+                overflow-x: auto;
+              }
 
-            .feeds-row {
-              overflow-x: auto;
-            }
+              .feeds-row .feeds-item {
+                flex: 1 1 100%;
+                min-width: 250px;
+              }
 
-            .feeds-row .feeds-item {
-              flex: 1 1 100%;
-              min-width: 250px;
-            }
+              .feeds-row .feeds-content {
+                text-align: center;
+                width: 100%;
+              }
+            `);
+          }
 
-            .feeds-row .feeds-content {
-              text-align: center;
-              width: 100%;
-            }
-          `);
+          feedsContainer.className = `widget-feeds feeds-${settings.direction}`;
+          feedsContainer.innerHTML = "";
+
+          data.feed.entry.forEach(entry => {
+            const itemData = getFeedItemData(entry, settings);
+            feedsContainer.innerHTML += replaceTemplateVars(itemTemplate, itemData);
+          });
         }
-
-        feedsContainer.className = `widget-feeds feeds-${settings.direction}`;
-        feedsContainer.innerHTML = "";
-
-        data.feed.entry.forEach(entry => {
-          const itemData = getFeedItemData(entry, settings);
-          feedsContainer.innerHTML += replaceTemplateVars(itemTemplate, itemData);
-        });
-      }
-    };
+      };
+    });
   }
 
   function injectStyles(css) {
