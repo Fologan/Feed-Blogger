ed9e65f19340efca0cc120e93da61fb6a3a7f4f6
Fologan <164580328+Fologan@users.noreply.github.com>
2024-03-31 08:06:16 -0600

    Update any-feed.js

--- any-feed.js
+++ any-feed.js
@@ -1,105 +1,79 @@
-//v0.6
-var Feeds = (function() {
-    "use strict";
+//v0.9
+    function getPostLink(entry) {
+        return entry.link.find(link => link.rel === 'alternate').href;
+    }
 
-    // Define la configuración por defecto para el plugin
-    const defaultSettings = {
-        url: "https://www.tvimperia.com/",
-        max: 4,
-        direction: "column",
-        background: "none",
-        title: "var(--color-text)",
-        category: "var(--inverse)",
-        categorybg: "var(--primary)",
-        border: "var(--primary)",
-        label: "farandula"
-    };
+    function isYoutubeUrl(url) {
+        if (url == null || typeof url !== 'string') return false
 
-    // Función para obtener la URL de cada entrada del feed
-    function getFeedUrl(entry) {
-        for (const link of entry.link) {
-            if (link.rel === "alternate") {
-                return link.href;
+        return url.includes('img.youtube.com')
+    }
+
+    function getPostImage(entry) {
+        let imgUrl = entry.media$thumbnail ? entry.media$thumbnail.url : '';
+
+        if (isYoutubeUrl(imgUrl)) {
+            // Si es una URL de YouTube, cambia a una calidad de imagen mayor.
+            return imgUrl.replace('default', 'mqdefault');
+        } else {
+            // Verifica si la URL incluye especificaciones de ancho y altura.
+            const hasWidthAndHeight = /-w\d+-h\d+(-c)?/.test(imgUrl);
+            if (hasWidthAndHeight) {
+                // Si la URL incluye especificaciones de ancho y altura, primero elimina cualquier prefijo de tamaño (ej. "s72-"),
+                // luego reemplaza con el nuevo tamaño deseado.
+                imgUrl = imgUrl.replace(/s\d+-?-w\d+-h\d+(-c)?/, "-w666-h375-c");
+            } else {
+                // Si la URL solo incluye especificación de altura (o potencialmente ninguna), ajusta solo la altura.
+                // Esto cubre los casos donde la URL es del tipo "/s72-c" o similar.
+                imgUrl = imgUrl.replace(/\/s\d+(-c)?/, "/h375"); // Usa "/h375-c" para incluir recorte.
             }
+            return imgUrl;
         }
     }
 
-    // Función para obtener los datos necesarios de cada entrada del feed
-    function getFeedItemData(entry, settings) {
-        const title = entry.title.$t;
-        const url = getFeedUrl(entry);
-        let category = "No Category";
-        if (entry.category && entry.category.length > 0) {
-            category = entry.category[0].term;
+
+    async function createSection(container, tag, postCount) {
+        try {
+            const response = await fetch('/feeds/posts/default/-/' + tag + '?alt=json&max-results=' + postCount);
+
+            if (!response.ok) {
+                throw new Error('Error al cargar las entradas');
+            }
+
+            const data = await response.json();
+            // Usa postCount para determinar cuántas entradas mostrar
+            const entries = data.feed.entry
+
+            const fragment = document.createDocumentFragment();
+
+            entries.forEach(function (entry) {
+                const title = entry.title.$t;
+                const link = getPostLink(entry);
+                const imageUrl = getPostImage(entry);
+
+                // Crear un div para cada entrada
+                const post = document.createElement('div');
+                post.classList.add('filter-tags'); // Agrega una clase para estilizar si es necesario
+
+                // Insertar cuerpo del post incluyendo la imagen si está disponible
+                post.innerHTML = (imageUrl ? `<div class='imagen-container'><a href="${link}"><img src="${imageUrl}" alt="Portada del post"></a></div>` : '') +
+                    `<div class='filter-tags-title-container'><h3 class='filter-tags-title'><a href="${link}">${title}</a></h3></div>`;
+
+                fragment.appendChild(post);
+            });
+
+            container.appendChild(fragment);
+        } catch (error) {
+            console.error(error.message);
         }
-        return { title, url, category };
     }
 
-    // Función para inyectar los estilos necesarios para el widget
-    function injectStyles() {
-        const style = document.createElement("style");
-        style.textContent = `
-            .widget-feeds {
-                display: flex;
-                flex-direction: ${defaultSettings.direction};
-                background: ${defaultSettings.background};
-                border: 1px solid ${defaultSettings.border};
-                padding: 10px;
-            }
-            .widget-feeds a {
-                text-decoration: none;
-                color: ${defaultSettings.title};
-            }
-            .feeds-item {
-                margin-bottom: 10px;
-            }
-            .feeds-category {
-                background: ${defaultSettings.categorybg};
-                color: ${defaultSettings.category};
-                padding: 2px 5px;
-                border-radius: 5px;
-            }
-        `;
-        document.head.appendChild(style);
-    }
+    const elements = document.querySelectorAll('[data-section-label]');
 
-    // Función para reemplazar las variables en el template del ítem del feed
-    function replaceTemplateVars(template, vars) {
-        return template.replace(/\{\{(.*?)\}\}/g, (_, key) => vars[key]);
-    }
+    elements.forEach(async function (el) {
+        const category = el.dataset.sectionLabel;
+        // Utiliza el valor de data-number-post o un valor predeterminado si no se proporciona
+        const postCount = parseInt(el.dataset.numberPost, 10) || 9; // Usa 9 como valor predeterminado
 
-    // Función principal para inicializar el plugin
-    function initPlugin(options) {
-        const settings = Object.assign({}, defaultSettings, options);
-        const container = document.querySelector(settings.container);
-        if (!container) {
-            console.error("Container not found");
-            return;
-        }
-
-        injectStyles();
-
-        const script = document.createElement("script");
-        script.src = `${settings.url}feeds/posts/default/-/${settings.label}?alt=json-in-script&max-results=${settings.max}&callback=Feeds.displayFeed`;
-        document.body.appendChild(script);
-
-        window.Feeds = window.Feeds || {};
-        window.Feeds.displayFeed = function(data) {
-            const entries = data.feed.entry;
-            if (!entries) {
-                console.error("No entries found");
-                return;
-            }
-            container.innerHTML = entries.map(entry => {
-                const itemData = getFeedItemData(entry, settings);
-                const itemTemplate = `<div class='feeds-item'>
-                    <a class='feeds-link' target="_blank" href='${itemData.url}'>${itemData.title}</a>
-                    <div class='feeds-category'>${itemData.category}</div>
-                </div>`;
-                return replaceTemplateVars(itemTemplate, itemData);
-            }).join('');
-        };
-    }
-
-    return { initPlugin };
-})();
+        await createSection(el, category, postCount);
+    })
